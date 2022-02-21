a07bddeec592f832855e08419cc1d64812056a2d
Thomas Hartmann <40410024+ThomasHartmannDev@users.noreply.github.com>
2022-02-21 02:01:45 +0100

    Update feedback.py

--- Cogs/users/feedback.py
+++ Cogs/users/feedback.py
@@ -59,11 +59,11 @@ class Feedback(commands.Cog):
                 await channel.send(f"{ctx.message.guild.default_role} **Reclamação ou Feedback** {ctx.author.mention}")
                 fim = discord.Embed(color=0xFF0000)
                 fim.set_author(name=f"Reclamação / Feedback {ctx.author.name}")
-                fim.add_field(name=f"{msg1.content}",
+                fim.add_field(name=f"⠀⠀⠀",
                               value="⠀⠀⠀", inline=False)
                 fim.set_footer(text="Khal Special Forces Feedback", icon_url="https://i.imgur.com/c7fWRRb.png")
                 await channel.send(embed=fim)
-                #await channel.send(msg1.content)
+                await channel.send(msg1.content)
 
             except Exception as e:
                 embed = discord.Embed(color=0xFF0000)
